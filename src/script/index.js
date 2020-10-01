55a107a23c855ba2b5ba171128c6c8a81efddfcd
Marina Avinkina <marityz@gmail.com>
2020-10-01 11:16:56 +0300

    Настроен webpack, первичная версия верстки двух страниц

---
+++ src/script/index.js
@@ -0,0 +1,55 @@
+import './../pages/index.css';
+import Popup from "./Popup";
+
+(function () {
+    const popupRegistr = document.querySelector('.popup-reg');
+    const openPopupRegistrButton = document.querySelector('.header-menu__button');
+    const closePopupRegistrButton = popupRegistr.querySelector('.popup__close');
+
+    const popupAuth = document.querySelector('.popup-auth');
+    const openPopupAuth = popupRegistr.querySelector('.popup__form_auth-link');
+    const closePopupAuth = popupAuth.querySelector('.popup__close');
+
+
+    const togglePopupRegistr = new Popup(popupRegistr, closePopupRegistrButton, openPopupRegistrButton, ()=>{
+        console.log('llox')
+    });
+
+    togglePopupRegistr.setEventListeners();
+
+    const togglePopupAuth = new Popup( popupAuth, closePopupAuth, openPopupAuth, ()=>{
+        togglePopupRegistr.close();
+    });
+
+    togglePopupAuth.setEventListeners();
+
+
+
+
+
+
+
+
+})();
+
+
+
+
+
+
+
+
+// function toggleMenuMobile(){
+//     if (openNavMobileMenu.classList.contains('header-menu__nav-mobile_open')) {
+//         openNavMobileMenu.classList.remove('header-menu__nav-mobile_open');
+//         toggleNavMobileButton.style.backgroundImage = "url(../images/menuicon-mobile-menu.svg)"
+//     } else {
+//         openNavMobileMenu.classList.add('header-menu__nav-mobile_open');
+//         toggleNavMobileButton.style.backgroundImage = "url(../images/header-close.svg)"
+//     }
+// }
+//
+//
+// toggleNavMobileButton.addEventListener('click', () => {
+//     toggleMenuMobile()
+// });
