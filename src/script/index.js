3587000e4851d8f1c93f4ba712020b9506ea1452
Marina Avinkina <marityz@gmail.com>
2020-10-02 01:25:23 +0300

    Переделан  футер, навигация сделана списком

--- src/script/index.js
+++ src/script/index.js
@@ -1,43 +1,62 @@
 import './../pages/index.css';
 import Popup from "./Popup";
+import FormValidator from "./FormValidator";
+import MobileMenu from "./MobileMenu";
+import FormPopup from "./FormPopup";
 
 (function () {
-    const popupRegistr = document.querySelector('.popup-reg');
-    const openPopupRegistrButton = document.querySelector('.header-menu__button');
-    const closePopupRegistrButton = popupRegistr.querySelector('.popup__close');
 
+    const errorMessages = {
+        empty: 'Это обязательное поле',
+        wrongLength: 'Должно быть от 2 до 30 символов',
+        wrongUrl: 'Здесь должна быть ссылка',
+        wrongEmail: 'Здесь нужно указать email',
+        wrongPassword: 'Поле пароль должно содержать более 8 символов'
+    };
+    const textLoading = "Загрузка...";
+    //открытие/закрытие меню на мелких разрешениях
+    const mobileMenu = document.querySelector('.header-menu__nav-mobile');
+    const toggleButtonMobileMenu = document.querySelector('.header-menu__nav-mobile-toggle-button');
 
-    const popupAuth = document.querySelector('.popup-auth');
-    const openPopupAuth = popupRegistr.querySelector('.popup__form_auth-link');
-    const closePopupAuth = popupAuth.querySelector('.popup__close');
+    const toggleMobileMenu = new MobileMenu(mobileMenu, toggleButtonMobileMenu);
+    toggleMobileMenu.setEventListeners();
 
+    // все о попапе регистрации
+    const popupRegistration = document.querySelector('.popup-reg');
+    const openPopupRegistrationButton = document.querySelector('.header-menu__button');
+    const closePopupRegistrationButton = popupRegistration.querySelector('.popup__close');
+    const formRegistration = document.forms.registr;
+    const validFormRegistration = new FormValidator(formRegistration, errorMessages);
 
-    const togglePopupRegistr = new Popup(popupRegistr, closePopupRegistrButton, openPopupRegistrButton, ()=>{
-        console.log('llox')
+    function setSubmitButtonUnactive(form, formValid) {
+        formValid.resetErrorForm();
+        form.reset();
+        formValid.setSubmitButtonState(false);
+    }
+
+    const togglePopupRegistration = new Popup(popupRegistration, closePopupRegistrationButton, openPopupRegistrationButton, () => {
+        setSubmitButtonUnactive(formRegistration, validFormRegistration)
     });
 
-    togglePopupRegistr.setEventListeners();
+    togglePopupRegistration.setEventListeners();
 
-    const togglePopupAuth = new Popup( popupAuth, closePopupAuth, openPopupAuth, ()=>{
-        togglePopupRegistr.close();
+
+//все о папапе авторизации
+    const popupAuth = document.querySelector('.popup-auth');
+    const openPopupAuth = popupRegistration.querySelector('.popup__form_auth-link');
+    const closePopupAuth = popupAuth.querySelector('.popup__close');
+    const formAuth = document.forms.auth;
+    const validFormAuth = new FormValidator(formAuth, errorMessages);
+
+
+    const togglePopupAuth = new Popup(popupAuth, closePopupAuth, openPopupAuth, () => {
+        togglePopupRegistration.close();
     });
 
     togglePopupAuth.setEventListeners();
 
 
-
-
-
-
-
-
 })();
-
-
-
-
-
-
 
 
 // function toggleMenuMobile(){
